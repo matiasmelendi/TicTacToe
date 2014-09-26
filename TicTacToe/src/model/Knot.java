e1b0380f9cbfa29a50de1bea8475930f6bcbaa09
Matias <mmelendi@10pines.com>
2014-09-26 12:01:32 -0300

    Marks added

---
+++ TicTacToe/src/model/Knot.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Knot extends Mark {
+
+	public Knot() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean isUnmarked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
