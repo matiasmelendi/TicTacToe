8e834fe1d0abb18c71db40726431003dcab8ac6c
Matias <mmelendi@10pines.com>
2014-09-26 11:07:24 -0300

    First test failing!

---
+++ TicTacToe/src/model/TicTacToe.java
@@ -0,0 +1,14 @@
+package model;
+
+public class TicTacToe {
+
+	public TicTacToe(Player player1, Player player2) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean boardIsEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
--- TicTacToe/test/model/TicTacToeTest.java
+++ TicTacToe/test/model/TicTacToeTest.java
@@ -7,13 +7,18 @@ import org.junit.Test;
 
 public class TicTacToeTest {
 
+	Player player1;
+	Player player2;
+	
 	@Before
 	public void setUp() throws Exception {
+		player1= new HumanPlayer();
+		player2= new HumanPlayer();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testTicTacToeStartEmpty() {
+		assertTrue(new TicTacToe(player1,player2).boardIsEmpty());
 	}
 
 }
