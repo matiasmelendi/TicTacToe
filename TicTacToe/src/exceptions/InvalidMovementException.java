2cee15164a95cf181786c3e30e6e49ffb4f5a2f4
Matias <mmelendi@10pines.com>
2014-09-29 17:49:14 -0300

    BoardStates added! All tested.

--- TicTacToe/src/exceptions/InvalidMovementException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-public class InvalidMovementException extends Exception {
-
-	public InvalidMovementException() {
-		super("Invalid movement, the position should be between (0,0) and (2,2)");
-	}
-
-}
--- TicTacToe/src/model/HumanPlayer.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class HumanPlayer extends Player {
-
-	public HumanPlayer(Mark mark) {
-		super(mark);
-	}
-
-}
